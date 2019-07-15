9fd8d042b8c5e564d520857f7337e7e50357571d
Ferenc Janky <fecjanky@gmail.com>
2019-07-15 21:55:01 +0200

    added login template

--- webui/src/main.js
+++ webui/src/main.js
@@ -1,7 +1,12 @@
-
 const express = require('express')
 const app = express()
+var path = require("path");
 const port = 3000
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
 
 var fs = require("fs");
 var Web3 = require("web3");
@@ -11,12 +16,45 @@ var abi = require("./ballot.json")
 let web3 = new Web3(Web3.providers.WebsocketProvider('ws://localhost:8546'));
 var contractInstance;
 
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// serve static files from template
+app.use(express.static(__dirname + '/webTemplate'));
+
+// include routes
+var routes = require('./routes/router');
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+app.listen(port, () => console.log(`Example app listening on http://localhost:${port}!`))
+
+
+
+/*
 app.get('/', (req, response) => {
+	return response.sendFile(path.join(__dirname + '/../web/index.html'));
 	    fs.readFile('../index.html',function (err, data){
 		            response.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
 		            response.write(data);
 		            response.end();
 		        });
+		        
 })
 
 function sendJSON(response,obj){
@@ -49,4 +87,5 @@ app.get('/getWinner',(req,res) =>{
 	.catch(error => console.log(error))
 });
 
-app.listen(port, () => console.log(`Example app listening on http://localhost:${port}!`))
+*/
+
