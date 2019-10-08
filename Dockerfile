c4557f41d7cae043158ab6b368b3041a50c5d8c7
Ferenc Janky <fecjanky@gmail.com>
2019-10-08 22:05:11 +0200

    use https server

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,20 @@
 FROM ubuntu:bionic
+
 ARG GENESIS_PASSWORD
+
 ENV MINER_THREADS=1
-RUN apt-get update && apt-get install -y wget curl nodejs mongodb build-essential npm git vim && npm install -g npm@latest && npm install -g node-pre-gyp@latest
+
+RUN apt-get update && apt-get install -y wget curl nodejs mongodb build-essential npm git vim openssl && npm install -g npm@latest && npm install -g node-pre-gyp@latest
+
 WORKDIR /iot_sc_tutorial/
 COPY ./ ./
-RUN npm install node-pre-gyp -g && cd webui && npm install && cd - && ./install_geth && ./init_geth ${GENESIS_PASSWORD}
-EXPOSE 3000
+
+RUN npm install node-pre-gyp -g && cd webui && npm install && cd - && ./install_geth && ./init_geth ${GENESIS_PASSWORD} && ./webui/scripts/gencerts.sh
+
+EXPOSE 8080/tcp
+EXPOSE 8080/udp
+EXPOSE 8443/tcp
+EXPOSE 8443/udp
+
 CMD ["./start.sh"]
 
--- webui/src/main.js
+++ webui/src/main.js
@@ -1,14 +1,26 @@
 const express = require('express')
+const https = require('https');
+let http = require('http');
 const app = express()
 let path = require("path");
 const listen_addr = "0.0.0.0";
-const listen_port = 3000;
+const listen_port = 8443;
+const listen_port_http = 8080;
 let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
 let session = require('express-session');
 let MongoStore = require('connect-mongo')(session);
 let fs = require("fs");
 let formidableMiddleware = require('express-formidable');
+
+let key = fs.readFileSync(path.join(__dirname, '..', 'certs', 'selfsigned.key'));
+let cert = fs.readFileSync(path.join(__dirname, '..', 'certs', 'selfsigned.crt'));
+
+var options = {
+	key: key,
+	cert: cert
+};
+
 
 //connect to MongoDB
 mongoose.connect('mongodb://localhost:27018/auth', { useNewUrlParser: true, useCreateIndex: true });
@@ -50,6 +62,15 @@ app.use(function (err, req, res, next) {
 	res.send(err.message);
 });
 
-app.listen(listen_port, listen_addr, () => console.log(`Example app listening on http://${listen_addr}:${listen_port}!`))
+let server = https.createServer(options, app);
+
+server.listen(listen_port, listen_addr, () => console.log(`Example app listening on https://${listen_addr}:${listen_port}!`))
+
+// Redirect from http port 80 to https
+http.createServer(function (req, res) {
+	let host = req.headers['host'].split(':')[0];
+	res.writeHead(301, { "Location": `https://${host}:${listen_port}${req.url}` });
+	res.end();
+}).listen(listen_port_http, listen_addr, () => console.log(`HTTP server for redirect listening on http://${listen_addr}:${listen_port_http}!`));
 
 
