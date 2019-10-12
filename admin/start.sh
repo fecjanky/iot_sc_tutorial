85b78a93f2417d37a8fab22090da9f270723a31f
Ferenc Janky <fecjanky@gmail.com>
2019-10-12 17:58:26 +0200

    fixed logrotation

--- admin/start.sh
+++ admin/start.sh
@@ -16,12 +16,12 @@ GETH_LOG_FILE="$ROOT_DIR/log/geth.log"
 DB_LOG_FILE="$ROOT_DIR/log/db.log"
 WWW_LOG_FILE="$ROOT_DIR/log/www.log"
 
-savelog -r "$ROOT_DIR/log/" -n -c 7 "$GETH_LOG_FILE"
-savelog -r "$ROOT_DIR/log/" -n -c 7 "$DB_LOG_FILE"
-savelog -r "$ROOT_DIR/log/" -n -c 7 "$WWW_LOG_FILE"
+savelog -n -c 7 "$GETH_LOG_FILE"
+savelog -n -c 7 "$DB_LOG_FILE"
+savelog -n -c 7 "$WWW_LOG_FILE"
 
-$WORK_DIR/start_geth --mine --miner.threads $MINER_THREADS | tee "$GETH_LOG_FILE" &
-$ROOT_DIR/webui/scripts/start_db.sh | tee "$DB_LOG_FILE" &
-node $ROOT_DIR/webui/src/main.js --listen 80 --https 443 | tee "$WWW_LOG_FILE" &
+$WORK_DIR/start_geth --mine --miner.threads $MINER_THREADS 2>&1 | tee "$GETH_LOG_FILE" &
+$ROOT_DIR/webui/scripts/start_db.sh 2>&1 | tee "$DB_LOG_FILE" &
+node $ROOT_DIR/webui/src/main.js --listen 80 --https 443 2>&1 | tee "$WWW_LOG_FILE" &
 
 wait
