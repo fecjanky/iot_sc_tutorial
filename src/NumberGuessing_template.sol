ecc4bfb575e32853ab11b6ab0a8f78fd668709e2
Ferenc Janky <fecjanky@gmail.com>
2019-10-14 07:25:08 +0200

    number guessing template

---
+++ src/NumberGuessing_template.sol
@@ -0,0 +1,24 @@
+pragma solidity >=0.4.22 <0.7.0;
+
+contract NumberGuessingGame {
+    constructor(uint maxNumberOfGuesses_) public payable {
+        /* Implement*/
+    }
+
+    function guess(uint guess_) public {
+        /* Implement*/
+    }
+
+    function forceEndGame() public
+    {
+        /* Implement*/
+    }
+
+    function result() public view returns(uint,address) {
+        /* Implement*/
+    }
+
+    function withdraw() public{
+        /* Implement*/
+    }
+}
